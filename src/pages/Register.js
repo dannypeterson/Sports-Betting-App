2ba3ddb587d7c85beb990f39d706badedaacc439
Danny <dannypeterson28@gmail.com>
2023-02-10 18:07:45 -0500

    demo button works

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -48,7 +48,7 @@ const Register = () => {
           className="already-registered-button"
           onClick={() => navigate('/home')}
         >
-          <p>Already have an account?</p>
+          <p>Use Demo</p>
         </div>
         <form className="register-form" onSubmit={handleSubmit}>
           {/* email label  */}
