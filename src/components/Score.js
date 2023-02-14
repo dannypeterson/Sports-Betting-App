ef6b8ee950521dde5ad781e005c2ec45dadb0dac
Danny <dannypeterson28@gmail.com>
2023-02-14 15:00:36 -0500

    scores page updated with formated logos

--- src/components/Score.js
+++ src/components/Score.js
@@ -4,22 +4,26 @@ const Scores = ({ scores }) => {
       {scores?.map((score) => (
         <div className="scores-map" key={score.gameId}>
           <div>
-            <img
-              className="team-logo"
-              src={`../../assets/EPL-Team-Logos/${score.away_team[0].name}.png`}
-            ></img>
-            <h3>
-              {score.away_team[0].name}{' '}
-              <span className="score">{score.away_team[0].score}</span>
-            </h3>
-            <img
-              className="team-logo"
-              src={`../../assets/EPL-Team-Logos/${score.home_team[0].name}.png`}
-            ></img>
-            <h3>
-              {score.home_team[0].name}{' '}
-              <span className="score">{score.home_team[0].score}</span>
-            </h3>
+            <section className="away-scores">
+              <img
+                className="team-logo"
+                src={`../../assets/EPL-Team-Logos/${score.away_team[0].name}.png`}
+              ></img>
+              <h3>
+                {score.away_team[0].name}{' '}
+                <span className="score">{score.away_team[0].score}</span>
+              </h3>
+            </section>
+            <section className="home-scores">
+              <img
+                className="team-logo"
+                src={`../../assets/EPL-Team-Logos/${score.home_team[0].name}.png`}
+              ></img>
+              <h3>
+                {score.home_team[0].name}{' '}
+                <span className="score">{score.home_team[0].score}</span>
+              </h3>
+            </section>
           </div>
           <p>
             {score.completed === true ? (
